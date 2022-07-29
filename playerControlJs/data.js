a872d2f0311f8022e15832dbf8ce1c85c4abef92
Francisco Messias C. Assunção <102373188+FMessiasCA@users.noreply.github.com>
2022-07-29 01:18:27 -0400

    Add files via upload

--- playerControlJs/data.js
+++ playerControlJs/data.js
@@ -1,20 +1,63 @@
 export default [
-{
-  title  : "Hino do Real Madrid",
-  artist : "Antonio S. / Marino G. / Mercedes F.",
-  file   : "../files/halaMadrid.mp3"
-},
+  
+  {
+    title  : "Falling Together",
+    artist : "ARTIFICIAL.MUSIC, SYIPHOROUS",
+    file   : "../files/songs/falling _Together.mp3"
+  },
+  
+  {
+    title  : "Sunday Vibe",
+    artist : "Kyoto",
+    file   : "../files/songs/sunday_vibe-kyoto.mp3"
+  },
+  
+  {
+    title  : "Bake A Pie",
+    artist : "Lukrembo",
+    file   : "../files/songs/bake_a_Pie-Lukrembo.mp3"
+  },
+  
+  {
+    title  : "Chill Vibes",
+    artist : "Kyoto",
+    file   : "../files/songs/chill_Vibes-kyoto.mp3"
+  },
 
-{
-  title  : "Gol do Vinicius Jr na final da Champions",
-  artist : "Narração espanhol",
-  file   : "../files/golDoVini.mp3"
-},
-
-{
-  title  : "Um homem, uma maquina, uma besta enjaulada",
-  artist : "Cristiano Ronaldo",
-  file   : "../files/umaBestaEnjaulada.mp3"
-}
-
-];
+  {
+    title  : "Balloon",
+      artist : "Kyoto",
+      file   : "../files/songs/balloon-kyoto.mp3"
+  },
+  
+  {
+    title  : "Coffee Break",
+    artist : "Kyoto",
+    file   : "../files/songs/coffee_Break-kyoto.mp3"
+  },
+  
+  {
+    title  : "Island",
+    artist : "Jarico",
+    file   : "../files/songs/island-jarico.mp3"
+  },
+  
+  {
+    title  : "Waiting",
+    artist : "INOSSO",
+    file   : "../files/songs/waiting-INOSSO.mp3"
+  },
+  
+/*   {
+    title  : "***",
+    artist : "***",
+    file   : "../files/songs/***"
+  },
+  
+  {
+    title  : "***",
+    artist : "***",
+    file   : "../files/songs/***"
+  }, */
+  
+  ];
